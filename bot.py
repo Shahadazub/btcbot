2b8f7f575a20ee8cb747d9700df3d3b498509395
Andrey Helix <andreigalkine@gmail.com>
2018-01-23 21:13:27 +0300

    added recurrent recieving of updates

--- bot.py
+++ bot.py
@@ -1,10 +1,12 @@
 import requests
+from time import sleep
 
 url = "https://api.telegram.org/bot295591274:AAGy-L97NEdYzw4SwdqXjvC3evOXB5fVwOs/"
 
 
 def get_updates_json(request):  
-    response = requests.get(request + 'getUpdates')
+    params = {'timeout': 100, 'offset': None}
+    response = requests.get(request + 'getUpdates', data=params)
     return response.json()
 
 
@@ -23,5 +25,21 @@ def send_mess(chat, text):
     response = requests.post(url + 'sendMessage', data=params)
     return response
 
-chat_id = get_chat_id(last_update(get_updates_json(url)))
-send_mess(chat_id, 'Your message goes here')
+def main():  
+    update_id = last_update(get_updates_json(url))['update_id']
+    while True:
+        if update_id == last_update(get_updates_json(url))['update_id']:
+           send_mess(get_chat_id(last_update(get_updates_json(url))), 'test')
+           update_id += 1
+        sleep(1)       
+
+def main():  
+    update_id = last_update(get_updates_json(url))['update_id']
+    while True:
+        if update_id == last_update(get_updates_json(url))['update_id']:
+           send_mess(get_chat_id(last_update(get_updates_json(url))), 'test')
+           update_id += 1
+        sleep(1)       
+
+if __name__ == '__main__':  
+    main()
